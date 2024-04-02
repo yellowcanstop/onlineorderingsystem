517099fed9a50ef0c985b84b1793b667e3620801
yellowcanstop <yellowcanstop@gmail.com>
2024-04-02 23:09:19 +0800

    Fixed bugs on profile.php

--- cart/profile.php
+++ cart/profile.php
@@ -33,7 +33,7 @@ if (isset($_POST['new_email'])) {
 		$stmt->bindValue(':email', $_POST['new_email'], PDO::PARAM_STR);
 		$stmt->execute();
 		$email = $stmt->fetch(PDO::FETCH_ASSOC);
-		if ($_POST['new_email'] == $email['email']) {
+		if (!empty($email)) {
 			$_SESSION['error'] = 'Email already exists.';
 			header('Location: index.php/page=profile');
 			exit();
@@ -54,7 +54,7 @@ if (isset($_POST['new_email'])) {
 	}
 }
 
-if (isset($POST['new_password'])) {
+if (isset($_POST['new_password'])) {
 	$current_password = $_POST['current_password'];
 	$new_password = $_POST['new_password'];
 	if ($stmt = $pdo -> prepare('SELECT password FROM accounts WHERE account_id = :account_id')) {
@@ -64,23 +64,23 @@ if (isset($POST['new_password'])) {
 		if (password_verify($current_password, $password['password']) && ($current_password != $new_password)) {
 			if ($stmt = $pdo -> prepare('UPDATE accounts SET password = :password WHERE account_id = :account_id')) {
 				$stmt->bindValue(':password', password_hash($new_password, PASSWORD_DEFAULT), PDO::PARAM_STR);
-				$stmt->bindValue(':id', $_SESSION['account_id'], PDO::PARAM_INT);
+				$stmt->bindValue(':account_id', $_SESSION['account_id'], PDO::PARAM_INT);
 				$stmt->execute();
 			} else {
 				$_SESSION['error'] = 'Cannot update password. Please try again.';
-				header('Location: index.php/page=profile');
+				header('Location: index.php?page=profile');
 				error_log('Cannot prepare sql statement for accounts table.');
 				exit();
 			}
 		} else {
 			$_SESSION['error'] = 'Your current password is incorrect or your new password is the same as your old one. Please try again.';
-			header('Location: index.php/page=profile');
+			header('Location: index.php?page=profile');
 			exit();
 		}
 	}
 }
 
-if (isset($POST['new_phone'])) {
+if (isset($_POST['new_phone'])) {
 	$new_phone = $_POST['new_phone'];
 	if ($stmt = $pdo -> prepare('UPDATE customers SET customer_phone = :phone WHERE account_id = :account_id')) {
 		$stmt->bindValue(':phone', $new_phone, PDO::PARAM_STR);
@@ -169,8 +169,10 @@ function checkPassword() {
         url: 'checkpassword.php',
         type: 'POST',
         data: {password: password},
+		dataType: 'text',
         success: function(response) {
-            if (response == 'invalid') {
+            response = response.trim();
+			if (response == 'invalid') {
                 $('#password').css('border', '3px solid red');
                 passwordCheck.textContent = 'Password must be between 8 and 20 characters long, contain letters, numbers and at least one special character!';
                 passwordCheck.style.color = 'red';
