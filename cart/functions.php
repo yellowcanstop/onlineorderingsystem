1dcad57479ece52395fe51b5ab153bd249353a44
yellowcanstop <yellowcanstop@gmail.com>
2024-04-12 21:36:28 +0800

    Customized header for employee

--- cart/functions.php
+++ cart/functions.php
@@ -26,15 +26,18 @@ function start_session() {
 function template_header($title) {
 $num_items_in_cart = isset($_SESSION['cart']) ? ($_SESSION['cart']['num_items_in_cart']) : 0;
 $name = isset($_SESSION['username']) ? (htmlspecialchars($_SESSION['username'], ENT_QUOTES)) : "";
-$greeting = isset($_SESSION['loggedin']) ? "<h2>Hi $name, what are you craving?</h2>" : "";
-$home = isset($_SESSION['loggedin']) ? "<a href=\"index.php\">Menu</a>" : "";
-$products = isset($_SESSION['loggedin']) ? "<a href=\"index.php?page=products\">Dishes</a>" : "";
-$profile = isset($_SESSION['loggedin']) ? "<a href=\"index.php?page=profile\">Profile</a>" : "";
-$orders = isset($_SESSION['loggedin']) ? "<a href=\"index.php?page=orders\">Orders</a>" : "";
+$greeting = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<h2>Hi $name, what are you craving?</h2>" : "";
+$home = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<a href=\"index.php\">Menu</a>" : "";
+$products = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<a href=\"index.php?page=products\">Dishes</a>" : "";
+$profile = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<a href=\"index.php?page=profile\">Profile</a>" : "";
+$orders = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<a href=\"index.php?page=orders\">Orders</a>" : "";
 $register = isset($_SESSION['loggedin']) ? "" : "<a href=\"register.php\">Register</a>";
 $login = isset($_SESSION['loggedin']) ? "" : "<a href=\"login.php\">Login</a>";
 $logout = isset($_SESSION['loggedin']) ? "<a href=\"logout.php\"><i class=\"fas fa-sign-out-alt\"></i> Logout</a>" : "";
-$cart = isset($_SESSION['loggedin']) ? "<a href=\"index.php?page=cart\"><i class=\"fas fa-shopping-cart\"></i><span>$num_items_in_cart</span></a>" : "";
+$cart = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'customer') ? "<a href=\"index.php?page=cart\"><i class=\"fas fa-shopping-cart\"></i><span>$num_items_in_cart</span></a>" : "";
+$manageorders = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'employee') ? "<a href=\"index.php?page=manageorders\">Manage Orders</a>" : "";
+$managedishes = isset($_SESSION['loggedin']) && ($_SESSION['role'] == 'employee') ? "<a href=\"index.php?page=managedishes\">Manage Inventory</a>" : "";
+
 echo <<<EOT
 <!DOCTYPE html>
 <html>
@@ -56,6 +59,8 @@ echo <<<EOT
                     $orders
                     $login
                     $register
+                    $manageorders
+                    $managedishes
                 </nav>
                 <div class="link-icons">$cart</div>
                 <div class="link-icons">$logout</div>
