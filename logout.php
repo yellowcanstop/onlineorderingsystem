822318af26671ce02d0f50bac4537bb6d195a1d4
yellowcanstop <yellowcanstop@gmail.com>
2024-04-16 23:12:52 +0800

    destroy cookie when logout

--- logout.php
+++ logout.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 session_destroy();
-// Redirect to the login page:
+// remove remember_me cookie
+// set value to empty string
+// set expiration to one hour in the past (-3600)
+if (isset($_COOKIE['remember_me'])) {
+    setcookie('remember_me', '', time() - 3600, '/');
+}
+// redirect to the login page:
 header('Location: login.php');
 ?>
