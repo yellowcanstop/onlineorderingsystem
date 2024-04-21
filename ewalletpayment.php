cafe1eff05a44c5bd9379f734e7c62acf531a1b2
mazshuky <72011583+mazshuky@users.noreply.github.com>
2024-04-21 13:23:23 +0800

    updated css for ewalletpayment.php

--- ewalletpayment.php
+++ ewalletpayment.php
@@ -1,11 +1,12 @@
 <?=template_header('E-Wallet Payment')?>
 <div class="product content-wrapper">
+    <div class="ewallet">
     <h1>Please scan below QR Code</h1>
     <div>
         <img src="imgs/qrcode.png" alt="TnG-QR" class="qr-code">
     </div>
     <br>
-    <form action="index.php?page=processpayment" method="post">       
+    <form action="index.php?page=processpayment" method="post" class="ewallet-form">       
         <input type="hidden" name="date_order_paid" value="<?= time() ?>" id="date_order_paid">
         <input type="submit" value="Continue">
     </form>
